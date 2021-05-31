8d65928725a8b6c7052c9dc0b6c0e6d600209756
ChustaD <chusta2013@mail.ru>
2021-05-31 17:26:36 +0300

    lazyMap and fix price

--- js/map.js
+++ js/map.js
@@ -1,29 +1,52 @@
 ;(function() {
-  if (typeof ymaps === 'undefined') {
-    return;
-  }
+  var sectionContacts = document.querySelector('.section-contacts');
 
-  ymaps.ready(function () {
-    var myMap = new ymaps.Map('ymap', {
-            center: [59.935710, 30.335065],
-            zoom: 16
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
+  var ymapInit = function() {
+    if (typeof ymaps === 'undefined') {
+      return;
+    }
+  
+    ymaps.ready(function () {
+      var myMap = new ymaps.Map('ymap', {
+              center: [59.935710, 30.335065],
+              zoom: 16
+          }, {
+              searchControlProvider: 'yandex#search'
+          }),
+  
+          myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+              balloonContent: 'г. Санкт-Петербург, Садовая, 7'
+          }, {
+              iconLayout: 'default#image',
+              iconImageHref: './img/marker.svg',
+              iconImageSize: [70, 70],
+              iconImageOffset: [-30, -70]
+          });
+  
+      myMap.geoObjects.add(myPlacemark);
+  
+      myMap.behaviors.disable('scrollZoom');
+    });
+  };
 
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            balloonContent: 'г. Санкт-Петербург, Садовая, 7'
-        }, {
-            iconLayout: 'default#image',
-            iconImageHref: './img/marker.svg',
-            iconImageSize: [70, 70],
-            iconImageOffset: [-30, -70]
-        });
+  var ymapLoad = function() {
+    var script = document.createElement('script');
+    script.src = 'https://api-maps.yandex.ru/2.1/?lang=en_RU';
+    myLib.body.appendChild(script);
+    script.addEventListener('load', ymapInit);
+  };
 
-    myMap.geoObjects.add(myPlacemark);
+  var checkYmapInit = function() {
+    var sectionContactsTop = sectionContacts.getBoundingClientRect().top;
+    var scrollTop = window.pageYOffset;
+    var sectionContactsOffsetTop = scrollTop + sectionContactsTop;
 
-    myMap.behaviors.disable('scrollZoom');
-});
+    if (scrollTop + window.innerHeight > sectionContactsOffsetTop) {
+      ymapLoad();
+      window.removeEventListener('scroll', checkYmapInit);
+    }
+  };
 
-
+  window.addEventListener('scroll', checkYmapInit);
+  checkYmapInit();
 })();
